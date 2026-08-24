6ed28eb0a2a2c5be196af08fe52779c92871521a
Tran Minh Triet <minhtriet.01102007@gmail.com>
2026-08-24 11:15:56 +0700

    add listening_history.cpp

---
+++ src/listening_history.cpp
@@ -0,0 +1,40 @@
+#include "listening_history.h"
+#include <iostream>
+
+Listening_History::Listening_History(){
+    history = new Song[n];
+    n = 50;
+    top = -1;
+}
+Listening_History::Listening_History(int n){
+    this->n = n;              
+    top = -1;
+    history = new Song[this->n];
+}
+Listening_History::~Listening_History(){
+    delete[] history;
+}
+void Listening_History::push(const Song& song){
+    if (top >= n-1){
+        std::cout << "Danh sach da day" << std::endl;
+        return; 
+    }
+    top++;                    
+    history[top] = song;
+
+}
+void Listening_History::pop(){
+    if (!empty()) top--; 
+    else  std::cout << "Lich su nghe dang rong" << std::endl;
+}
+Song* Listening_History::getTop() const{
+    if (empty()) return NULL;       
+    else return &history[top];     
+}
+bool Listening_History::empty() const{
+    return top == -1;
+}
+int Listening_History::getSize() const{
+    return top + 1;           
+}
+
