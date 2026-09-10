d6c9cd012e0df3eea1f80a46172aafb55b62595d
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-09-10 10:15:20 +0700

    Introduce NodeState enum in hashtablenode.h
    Added NodeState enum to represent the state of hash table nodes.

--- dsa/hashtable/hashtablenode.h
+++ dsa/hashtable/hashtablenode.h
@@ -1,7 +1,12 @@
 #pragma once
 
+enum NodeState{ 
+    EMPTY,      // Ô hoàn toàn trống
+    OCCUPIED,   // Ô đang chứa dữ liệu hợp lệ
+    DELETED     // Ô đã từng có dữ liệu nhưng bị xóa 
+};
 struct HashNode{
     int key;
     int value;
-    bool occupied;
+    NodeState state;
 };
